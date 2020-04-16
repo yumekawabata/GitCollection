d2bb78e4539322abf1c09e612e0f5a6c12cff298
ASW-研修３ <training03@ASW-kenshuu3noMacBook-Air.local>
2020-04-16 14:37:06 +0900

    create TopViewController

---
+++ Git Collection/Top/TopViewController.swift
@@ -0,0 +1,33 @@
+//
+//  TopViewController.swift
+//  Git Collection
+//
+//  Created by ASW-研修３ on 2020/04/16.
+//  Copyright © 2020 ASW-研修３. All rights reserved.
+//
+
+import UIKit
+import PGFramework
+// MARK: - Property
+class TopViewController: BaseViewController{
+}
+// MARK: - Life cycle
+extension TopViewController {
+    override func loadView() {
+        super.loadView()
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+}
+// MARK: - Protocol
+extension TopViewController {
+}
+// MARK: - method
+extension TopViewController {
+}
+
+
